85ac320d3a204e8ae28e1b23ef6bb296043c1322
Juan David Henao <jhenaoz93@gmail.com>
2015-09-20 13:27:01 -0500

    make changes in adoption, html from caro to work with angular js annotations

--- src/main/webapp/scripts/app/entities/adoption/adoption.controller.js
+++ src/main/webapp/scripts/app/entities/adoption/adoption.controller.js
@@ -2,36 +2,46 @@
 
 angular.module('petcareApp')
     .controller('AdoptionController', function ($scope, Adoption) {
-        $scope.adoptions = [];
-        $scope.loadAll = function() {
-            Adoption.query(function(result) {
-               $scope.adoptions = result;
-            });
-        };
-        $scope.loadAll();
-
-        $scope.delete = function (id) {
-            Adoption.get({id: id}, function(result) {
-                $scope.adoption = result;
-                $('#deleteAdoptionConfirmation').modal('show');
-            });
-        };
-
-        $scope.confirmDelete = function (id) {
-            Adoption.delete({id: id},
-                function () {
-                    $scope.loadAll();
-                    $('#deleteAdoptionConfirmation').modal('hide');
-                    $scope.clear();
-                });
-        };
-
-        $scope.refresh = function () {
-            $scope.loadAll();
-            $scope.clear();
-        };
-
-        $scope.clear = function () {
-            $scope.adoption = {name: null, species: null, age: null, gender: null, size: null, description: null, id: null};
-        };
+    	$scope.adoption = {};
+    	
+    	$scope.save = function(adoption){
+            if ($scope.adoption.id != null) {
+                Adoption.update($scope.adoption, onSaveFinished);
+            } else {
+                Adoption.save($scope.adoption, onSaveFinished);
+            }
+    	};
+    	
+//        $scope.adoptions = [];
+//        $scope.loadAll = function() {
+//            Adoption.query(function(result) {
+//               $scope.adoptions = result;
+//            });
+//        };
+//        $scope.loadAll();
+//
+//        $scope.delete = function (id) {
+//            Adoption.get({id: id}, function(result) {
+//                $scope.adoption = result;
+//                $('#deleteAdoptionConfirmation').modal('show');
+//            });
+//        };
+//
+//        $scope.confirmDelete = function (id) {
+//            Adoption.delete({id: id},
+//                function () {
+//                    $scope.loadAll();
+//                    $('#deleteAdoptionConfirmation').modal('hide');
+//                    $scope.clear();
+//                });
+//        };
+//
+//        $scope.refresh = function () {
+//            $scope.loadAll();
+//            $scope.clear();
+//        };
+//
+//        $scope.clear = function () {
+//            $scope.adoption = {name: null, species: null, age: null, gender: null, size: null, description: null, id: null};
+//        };
     });
