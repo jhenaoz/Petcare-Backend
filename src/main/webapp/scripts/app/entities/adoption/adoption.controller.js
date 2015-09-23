e86c0784d798381610b1412daa1403b2e8e416de
Juan David Henao <jhenaoz93@gmail.com>
2015-09-22 19:13:34 -0500

    solving issue with bad image.

--- src/main/webapp/scripts/app/entities/adoption/adoption.controller.js
+++ src/main/webapp/scripts/app/entities/adoption/adoption.controller.js
@@ -3,30 +3,51 @@
 angular.module('petcareApp')
     .controller('AdoptionController', function ($scope, Adoption, Principal, User) {
     	$scope.adoption = {};
+        $scope.adoptions = [];
 
-        Adoption.query(function(result) {
-           $scope.adoptions = result;
-        });
-        Principal.identity(true).then(function(response){
-            User.get({login:response.login}, function(currentUser){
-                $scope.user = currentUser;
-                $scope.adoption.have = currentUser;
+        $scope.init = function(){
+            Adoption.query(function(result) {
+               $scope.adoptions = result;
             });
-        })
+            Principal.identity(true).then(function(response){
+                User.get({login:response.login}, function(currentUser){
+                    $scope.user = currentUser;
+                    $scope.adoption.have = currentUser;
+                });
+            })            
+        };
+
     	
     	$scope.save = function(adoption){
             if ($scope.adoption.id != null) {
                 Adoption.update($scope.adoption, function(result){
                     console.log(result);
+                    $scope.adoptions.push(result);
+                    $scope.adoption = {};
                 });
             } else {
                 Adoption.save($scope.adoption, function(result){
                     console.log(result);
-                    alert('Guardado Exitoso');
+                    $scope.adoptions.push(result);
                     $scope.adoption = {};
                 });
             }
     	};
+       $scope.remove = function (id) {
+           Adoption.get({id: id}, function(result) {
+               $scope.adoption = result;
+               $('#deleteAdoptionConfirmation').modal('show');
+           });
+       };
+
+       $scope.confirmDelete = function (id) {
+           Adoption.delete({id: id},
+               function () {
+                   $('#deleteAdoptionConfirmation').modal('hide');
+                   $scope.init();
+               });
+       };
+    	
     	
     	
 //        $scope.adoptions = [];
@@ -37,21 +58,9 @@ angular.module('petcareApp')
 //        };
 //        $scope.loadAll();
 //
-//        $scope.delete = function (id) {
-//            Adoption.get({id: id}, function(result) {
-//                $scope.adoption = result;
-//                $('#deleteAdoptionConfirmation').modal('show');
-//            });
-//        };
+
 //
-//        $scope.confirmDelete = function (id) {
-//            Adoption.delete({id: id},
-//                function () {
-//                    $scope.loadAll();
-//                    $('#deleteAdoptionConfirmation').modal('hide');
-//                    $scope.clear();
-//                });
-//        };
+
 //
 //        $scope.refresh = function () {
 //            $scope.loadAll();
@@ -61,4 +70,5 @@ angular.module('petcareApp')
 //        $scope.clear = function () {
 //            $scope.adoption = {name: null, species: null, age: null, gender: null, size: null, description: null, id: null};
 //        };
+        $scope.init();
     });
