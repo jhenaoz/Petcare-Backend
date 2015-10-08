7c7b718c84160543f1c10b3f064e90673984f911
CaroP <carop1992@gmail.com>
2015-10-08 12:28:55 -0500

    Sponsor and lost view

--- src/main/webapp/scripts/app/entities/sponsor/sponsor.controller.js
+++ src/main/webapp/scripts/app/entities/sponsor/sponsor.controller.js
@@ -1,16 +1,44 @@
 'use strict';
 
 angular.module('petcareApp')
-    .controller('SponsorController', function ($scope, Sponsor) {
+    .controller('SponsorController', function ($scope, Sponsor, Principal, User) {
+    	$scope.sponsor = {};
         $scope.sponsors = [];
-        $scope.loadAll = function() {
+       /* $scope.loadAll = function() {
             Sponsor.query(function(result) {
                $scope.sponsors = result;
             });
         };
-        $scope.loadAll();
+        $scope.loadAll();*/
 
-        $scope.delete = function (id) {
+        $scope.init = function(){
+            Sponsor.query(function(result) {
+               $scope.sponsors = result;
+            });
+            Principal.identity(true).then(function(response){
+                User.get({login:response.login}, function(currentUser){
+                    $scope.user = currentUser;
+                    $scope.sponsor.have = currentUser;
+                });
+            })            
+        };
+    	
+    	$scope.save = function(sponsor){
+            if ($scope.sponsor.id != null) {
+            	Sponsor.update($scope.sponsor, function(result){
+                    console.log(result);
+                    $scope.sponsors.push(result);
+                    $scope.sponsor = {};
+                });
+            } else {
+            	Sponsor.save($scope.sponsor, function(result){
+                    console.log(result);
+                    $scope.sponsors.push(result);
+                    $scope.sponsor = {};
+                });
+            }
+    	};
+        /*$scope.delete = function (id) {
             Sponsor.get({id: id}, function(result) {
                 $scope.sponsor = result;
                 $('#deleteSponsorConfirmation').modal('show');
@@ -29,9 +57,27 @@ angular.module('petcareApp')
         $scope.refresh = function () {
             $scope.loadAll();
             $scope.clear();
+        };*/
+    	
+    	$scope.remove = function (id) {
+            Sponsor.get({id: id}, function(result) {
+                $scope.sponsor = result;
+                $('#deleteAdoptionConfirmation').modal('show');
+            });
+        };
+
+        $scope.confirmDelete = function (id) {
+            Sponsor.delete({id: id},
+                function () {
+                    $('#deleteSponsorConfirmation').modal('hide');
+                    $scope.init();
+                });
         };
 
         $scope.clear = function () {
             $scope.sponsor = {name: null, species: null, age: null, gender: null, size: null, description: null, image: null, id: null};
         };
+        
+        $scope.init();
+        
     });
