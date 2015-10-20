97974825b2adb2cf03768f09df52bd4ab7a2b0c4
CaroP <carop1992@gmail.com>
2015-10-19 22:00:04 -0500

    change image adoption and sponsor

--- src/main/webapp/scripts/app/entities/sponsor/sponsor-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/sponsor/sponsor-dialog.controller.js
@@ -28,4 +28,57 @@ angular.module('petcareApp').controller('SponsorDialogController',
         $scope.clear = function() {
             $modalInstance.dismiss('cancel');
         };
+        
+
+        $scope.abbreviate = function (text) {
+            if (!angular.isString(text)) {
+                return '';
+            }
+            if (text.length < 30) {
+                return text;
+            }
+            return text ? (text.substring(0, 15) + '...' + text.slice(-10)) : '';
+        };
+
+        $scope.byteSize = function (base64String) {
+            if (!angular.isString(base64String)) {
+                return '';
+            }
+            function endsWith(suffix, str) {
+                return str.indexOf(suffix, str.length - suffix.length) !== -1;
+            }
+            function paddingSize(base64String) {
+                if (endsWith('==', base64String)) {
+                    return 2;
+                }
+                if (endsWith('=', base64String)) {
+                    return 1;
+                }
+                return 0;
+            }
+            function size(base64String) {
+                return base64String.length / 4 * 3 - paddingSize(base64String);
+            }
+            function formatAsBytes(size) {
+                return size.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " bytes";
+            }
+
+            return formatAsBytes(size(base64String));
+        };
+
+        $scope.setImage = function ($files, lost) {
+            if ($files[0]) {
+                var file = $files[0];
+                var fileReader = new FileReader();
+                fileReader.readAsDataURL(file);
+                fileReader.onload = function (e) {
+                    var data = e.target.result;
+                    var base64Data = data.substr(data.indexOf('base64,') + 'base64,'.length);
+                    $scope.$apply(function() {
+                        lost.image = base64Data;
+                    });
+                };
+            }
+        };
+        
 }]);
